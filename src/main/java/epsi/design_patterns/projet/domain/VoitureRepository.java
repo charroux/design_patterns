9c59f21b1233d2f8d481858028199edd87fe8404
benoit.charroux <benoit.charroux@EFREI89124M2-P.campus.villejuif>
2018-06-11 17:13:15 +0200

    adding repositort

---
+++ src/main/java/epsi/design_patterns/projet/domain/VoitureRepository.java
@@ -0,0 +1,7 @@
+package epsi.design_patterns.projet.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface VoitureRepository extends CrudRepository<Voiture, String> {
+
+}
