8ae2d5230f8e07085eba8e6cf2866a08abf74b80
benoit.charroux <benoit.charroux@EFREI89124M2-P.campus.villejuif>
2018-06-18 09:18:52 +0200

    jpa voiture

--- src/main/java/epsi/design_patterns/DesignPatternsApplication.java
+++ src/main/java/epsi/design_patterns/DesignPatternsApplication.java
@@ -11,8 +11,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import epsi.design_patterns.jpaExemple.Customer;
-import epsi.design_patterns.jpaExemple.CustomerRepository;
+import epsi.design_patterns.projet.application.Conduire;
+import epsi.design_patterns.projet.application.ConduireTaxi;
 import epsi.design_patterns.projet.domain.Passager;
 import epsi.design_patterns.projet.domain.Voiture;
 import epsi.design_patterns.projet.domain.VoitureRepository;
@@ -29,6 +29,9 @@ public class DesignPatternsApplication {
 	@Bean
 	public CommandLineRunner demo1(VoitureRepository repository) {
 		return (args) -> {
+			
+			//Conduire c = new ConduireTaxi();
+			//c.addPassager("tintin");
 			
 			SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
 			Date miseEnCirculation = df.parse("30/05/2018");
@@ -50,44 +53,6 @@ public class DesignPatternsApplication {
 		};
 	}
 	
-	@Bean
-	public CommandLineRunner demo(CustomerRepository repository) {
-		return (args) -> {
-			// save a couple of customers
-			repository.save(new Customer("Jack", "Bauer"));
-			repository.save(new Customer("Chloe", "O'Brian"));
-			repository.save(new Customer("Kim", "Bauer"));
-			repository.save(new Customer("David", "Palmer"));
-			repository.save(new Customer("Michelle", "Dessler"));
-
-			// fetch all customers
-			log.info("Customers found with findAll():");
-			log.info("-------------------------------");
-			for (Customer customer : repository.findAll()) {
-				log.info(customer.toString());
-			}
-			log.info("");
-
-			// fetch an individual customer by ID
-			repository.findById(1L)
-				.ifPresent(customer -> {
-					log.info("Customer found with findById(1L):");
-					log.info("--------------------------------");
-					log.info(customer.toString());
-					log.info("");
-				});
-
-			// fetch customers by last name
-			log.info("Customer found with findByLastName('Bauer'):");
-			log.info("--------------------------------------------");
-			repository.findByLastName("Bauer").forEach(bauer -> {
-				log.info(bauer.toString());
-			});
-			// for (Customer bauer : repository.findByLastName("Bauer")) {
-			// 	log.info(bauer.toString());
-			// }
-			log.info("");
-		};
-	}
+	
 
 }
--- src/main/java/epsi/design_patterns/projet/application/ConduireTaxi.java
+++ src/main/java/epsi/design_patterns/projet/application/ConduireTaxi.java
@@ -1,16 +1,24 @@
 package epsi.design_patterns.projet.application;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import epsi.design_patterns.projet.domain.Passager;
 import epsi.design_patterns.projet.domain.Voiture;
+import epsi.design_patterns.projet.domain.VoitureRepository;
 import epsi.design_patterns.projet.services.persistence.Repository;
 
 public class ConduireTaxi implements Conduire{
 
+	@Autowired
+	VoitureRepository voitureRepository;
 	Voiture taxi = new Voiture();
 	Repository<Passager> repository;
 	
 	@Override
 	public Passager addPassager(String nomPassager) {
+		
+		System.out.println(voitureRepository);
+		
 		// rechercher passager existant
 		Passager passager = repository.findByNom(nomPassager);
 		// le passage n'existe pas
